d50cb955f965ea6610166e4ec575dae1c7835704
FlySkyFall <flyskyfalL@mail.ru>
2022-12-24 15:18:37 +0300

    final version

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def edit(id):
 
     return render_template('edit.html', post=post)
 
-@app.route('/<int:id>/delete', methods=('POST',))
+@app.route('/<int:id>/delete', methods=('GET','POST'))
 def delete(id):
     post = get_post(id)
     conn = get_db_connection()
